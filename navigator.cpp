59d03c2095cdc2c94b7306eff03be2d73031cbe5
Tri Dung Nguyen <tridung.190705@gmail.com>
2024-05-21 19:45:01 +0930

    finish merged

--- navigator.cpp
+++ navigator.cpp
@@ -10,8 +10,8 @@ Navigator::Navigator(QWidget *parent)
     // initialise database
     database = QSqlDatabase::addDatabase("QSQLITE", "DB1");
     //database.setDatabaseName("C:\\Users\\trung\\Downloads\\PlayerManagement.db");
-    //database.setDatabaseName("D:\\University of Adelaide\\COMP SCI 1102\\PlayerManagement.db");
-    database.setDatabaseName("/Users/tom/PlayerManagement.db");
+    database.setDatabaseName("D:\\University of Adelaide\\COMP SCI 1102\\PlayerManagement.db");
+    //database.setDatabaseName("/Users/tom/PlayerManagement.db");
 
     // check the database is working or not
     if(!database.open()){
